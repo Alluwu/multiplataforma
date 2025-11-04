3160d1120fce8db6f81786dd27825cc5934e6f9e
Alluwu <Allamguerra452@gmail.com>
2025-11-03 18:02:54 -0600

    Feat: Se agrega el proyecto base para los servicios correspondientes

---
+++ services/serviceProveedor/src/main/java/com/mp/proveedor/ServiceProveedorApplication.java
@@ -0,0 +1,11 @@
+package com.mp.proveedor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceProveedorApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceProveedorApplication.class, args);
+    }
+}
